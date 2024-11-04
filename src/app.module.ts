03beeb03a85a3d31d2dd27e1446aa28814d28ba7
NterChoi <choi16289@naver.com>
2024-11-04 20:06:38 +0900

    add : 초기 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { CardModule } from './card/card.module';
+import { ListModule } from './list/list.module';
+import { CommentModule } from './comment/comment.module';
+import { BoardModule } from './board/board.module';
+import { WorkspaceModule } from './workspace/workspace.module';
+import { ChecklistModule } from './checklist/checklist.module';
+import { ItemModule } from './item/item.module';
+import { FileModule } from './file/file.module';
+import { WorkspaceController } from './workspace/workspace.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule, CardModule, ListModule, CommentModule, BoardModule, UserModule, WorkspaceModule, ChecklistModule, ItemModule, FileModule],
+  controllers: [AppController, WorkspaceController],
   providers: [AppService],
 })
 export class AppModule {}
