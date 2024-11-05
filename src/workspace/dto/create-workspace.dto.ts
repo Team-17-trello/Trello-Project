beb912c362f76f98235fd5354fcdd450dc4fd71f
New-mean <dmb08075@naver.com>
2024-11-05 12:54:22 +0900

    add:workspace dto

--- src/workspace/dto/create-workspace.dto.ts
+++ src/workspace/dto/create-workspace.dto.ts
@@ -1,1 +1,11 @@
-export class CreateWorkspaceDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateWorkspaceDto {
+  @IsString()
+  @IsNotEmpty({ message: 'workspaceName을 입력해주세요.' })
+  workspaceName: string;
+
+  @IsNumber()
+  @IsNotEmpty({ message: 'userId를 입력해주세요.' })
+  userId: number;
+}
