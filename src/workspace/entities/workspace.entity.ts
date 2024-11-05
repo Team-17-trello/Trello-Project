029f29731ca3dfd87045c62f49b37e1e7a16f283
New-mean <dmb08075@naver.com>
2024-11-05 12:44:18 +0900

    add:workspace entity

--- src/workspace/entities/workspace.entity.ts
+++ src/workspace/entities/workspace.entity.ts
@@ -1,1 +1,16 @@
-export class WorkspaceEntity {}
+import { Column, Entity, PrimaryGeneratedColumn, Timestamp } from 'typeorm';
+
+@Entity({ name: 'workspace' })
+export class WorkspaceEntity {
+  @PrimaryGeneratedColumn()
+  workspaceId: number;
+
+  @Column({ type: 'varchar', nullable: false, name: 'workspaceName' })
+  workspaceName: string;
+
+  @Column({ type: 'timestamp', nullable: false, name: 'createdAt' })
+  createdAt: Timestamp;
+
+  @Column({ type: 'int', nullable: false, name: 'userId' })
+  userId: number;
+}
