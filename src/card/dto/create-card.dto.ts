570575e7b41d7339bc94ccc97d0297398ffdaee1
NterChoi <choi16289@naver.com>
2024-11-06 12:01:47 +0900

    add : card 엔티티, dto 작성

--- src/card/dto/create-card.dto.ts
+++ src/card/dto/create-card.dto.ts
@@ -1,1 +1,24 @@
-export class CreateCardDto {}
+import { IsDate, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateCardDto {
+  @IsNumber()
+  @IsNotEmpty({ message: '리스트 아이디를 입력해주세요' })
+  listId: number;
+
+  @IsString()
+  @IsNotEmpty({ message: '카드 제목을 입력해주세요' })
+  title: string;
+
+  @IsString()
+  @IsNotEmpty({ message: '카드 설명을 입력해주세요' })
+  description: string;
+
+  @IsString()
+  @IsNotEmpty({ message: '색상을 입력해주세요' })
+  color: string;
+
+  @IsDate()
+  @IsNotEmpty({ message: '마감일을 입력해주세요' })
+  dueDate: Date;
+
+}
--- src/card/entities/card.entity.ts
+++ src/card/entities/card.entity.ts
@@ -1,1 +1,32 @@
-export class CardEntity {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity({
+  name: 'cards',
+})
+export class CardEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', nullable: false })
+  title: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  description: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  color: string;
+
+  @Column({ type: 'int', nullable: false })
+  order: number;
+
+  @CreateDateColumn({ type: 'timestamp', nullable: false, name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', nullable: false, name: 'updated_at' })
+  updatedAt: Date;
+
+  @Column({ type: 'timestamp', nullable: false, name: 'due_date' })
+  dueDate: Date;
+
+
+}
