f3076d74d571b8a787d1d6b7056b6d6e728240be
NterChoi <choi16289@naver.com>
2024-11-07 20:46:33 +0900

    add : comment module imports 추가
    Co-authored-by:  surinnnnnn <didthfls2@naver.com>

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CommentController } from './comment.controller';
 import { CommentService } from './comment.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CardEntity } from '../card/entities/card.entity';
+import { CommentEntity } from './entities/comment.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CardEntity, CommentEntity])],
   controllers: [CommentController],
   providers: [CommentService]
 })
